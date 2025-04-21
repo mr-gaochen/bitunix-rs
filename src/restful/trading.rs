43e8f2172ddca2b05eb67db21f0390de62f73140
eric <mr.xinyi@yahoo.com>
2025-04-21 18:28:13 +0800

    update

--- src/restful/trading.rs
+++ src/restful/trading.rs
@@ -8,40 +8,37 @@ use std::collections::BTreeMap;
 impl BitUnixClient {
     ///  下单
     ///POST /api/v1/futures/trade/place_order
-    pub async fn trade_place_order<T>(
+    pub async fn trade_place_order(
         &self,
-        symbol: T,
-        qty: T,
-        side: T,
-        trade_side: Option<T>,
-        order_type: T,
-        price: Option<T>,
-        postion_id: Option<T>,
-        effect: Option<T>,
-        client_id: Option<T>,
-    ) -> Result<RestApi<TradePlceOrder>>
-    where
-        T: Into<String>,
-    {
+        symbol: String,
+        qty: String,
+        side: String,
+        trade_side: Option<String>,
+        order_type: String,
+        price: Option<String>,
+        postion_id: Option<String>,
+        effect: Option<String>,
+        client_id: Option<String>,
+    ) -> Result<RestApi<TradePlceOrder>> {
         let mut params: BTreeMap<String, Value> = BTreeMap::new();
-        params.insert("symbol".into(), json!(symbol.into()));
-        params.insert("qty".into(), json!(qty.into()));
-        params.insert("side".into(), json!(side.into()));
-        params.insert("orderType".into(), json!(order_type.into()));
+        params.insert("symbol".into(), json!(symbol));
+        params.insert("qty".into(), json!(qty));
+        params.insert("side".into(), json!(side));
+        params.insert("orderType".into(), json!(order_type));
         if let Some(trade_side) = trade_side {
-            params.insert("tradeSide".into(), json!(trade_side.into()));
+            params.insert("tradeSide".into(), json!(trade_side));
         }
         if let Some(price) = price {
-            params.insert("price".into(), json!(price.into()));
+            params.insert("price".into(), json!(price));
         }
         if let Some(postion_id) = postion_id {
-            params.insert("positionId".into(), json!(postion_id.into()));
+            params.insert("positionId".into(), json!(postion_id));
         }
         if let Some(effect) = effect {
-            params.insert("effect".into(), json!(effect.into()));
+            params.insert("effect".into(), json!(effect));
         }
         if let Some(client_id) = client_id {
-            params.insert("clientId".into(), json!(client_id.into()));
+            params.insert("clientId".into(), json!(client_id));
         }
         params.insert("reduceOnly".into(), json!(false));
 
