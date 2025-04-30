2d630f131c251edacb19c9c9142a14357935cd39
eric <mr.xinyi@yahoo.com>
2025-04-30 10:38:42 +0800

    update

--- src/restful/kline.rs
+++ src/restful/kline.rs
@@ -1,13 +1,19 @@
-use std::collections::BTreeMap;
-use anyhow::Result;
 use crate::client::BitUnixClient;
+use anyhow::Result;
+use std::collections::BTreeMap;
 
 use super::models::{KLineData, RestApi};
 
 impl BitUnixClient {
     /// 获取K线数据
     /// curl -X 'GET'  --location 'https://fapi.bitunix.com/api/v1/futures/market/kline?symbol=BTCUSDT&startTime=1&endTime=10234&interval=15m'
-    pub async fn futures_market_kline(&self, symbol: &str,start_time:u32,end_time:u32,interval:&str) -> Result<RestApi<KLineData>> {
+    pub async fn futures_market_kline(
+        &self,
+        symbol: &str,
+        start_time: i64,
+        end_time: i64,
+        interval: &str,
+    ) -> Result<RestApi<KLineData>> {
         let mut params: BTreeMap<String, String> = BTreeMap::new();
         params.insert("symbol".into(), symbol.into());
         params.insert("startTime".into(), start_time.to_string());
@@ -16,6 +22,5 @@ impl BitUnixClient {
         Ok(self
             .get::<RestApi<KLineData>>("/api/v1/futures/market/kline", &params)
             .await?)
-        
     }
 }
